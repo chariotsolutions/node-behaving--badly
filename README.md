dc41d98618486b792fda526fa9bc47de9f2252ec
Ken Rimple <krimple@chariotsolutions.com>
2016-06-21 19:00:10 -0400

    working functions and basic docs

--- README.md
+++ README.md
@@ -10,10 +10,43 @@ All of these things will block the event loop.
 
 Try this against the repo once you do an `npm install` and a `node server.js`:
 
-ab -n 10 http://localhost:3000/functionaldunctional
+ab -n 100 -i 6 http://localhost:3000/functionalnative
 
+* Runs 100 iterations, 6 at a time
+* Reports benchmark results after running
+
+You should see a big red flag here:
+
+```text
 Concurrency level: 1
+```
 
-Whoops!
+So a node engine will only allow one thing at a time to consume its CPU. If you're coming fom a thread or worker background, consider each node instance a worker.
 
-Node is a butler, not a worker.
+## To fix this situation you need a load balancer
+
+Node is a butler, not a worker.  You can install `pm2` (with `npm install -g pm2` and then fire up as many server instances as you have CPU nodes:
+
+```bash
+pm2 start server.js -i -1
+```
+
+Then you can monitor it with `pm2 monit` and fire up your requests, which will be spread across nodes.
+
+## Benchmark URLs
+
+First, our Name API test - maps over 100000 randomly generated names with `faker` - reports the count from A-M and N-Z.
+
+* `/functionalnative` - use the native built-in V8 Array.prototype.reduce
+* `/functionalramda` - use the RamdaJS library
+* `/functionalasync` - use the Async library and `process.nextTick`
+
+CPU test next - peg the CPU
+
+* `/tightloop` just burn up the CPU with a tight loop
+
+Memory test w/core dump - overload Node
+
+* `/growit` - keep pushing elements onto an array until it blows up
+
+
--- server.js
+++ server.js
@@ -3,6 +3,7 @@ var express = require('express');
 var app = express();
 var faker = require('faker');
 var R = require('ramda');
+var async = require('async');
 
 var names = [];
 for (let i = 0; i < 10000000; i++) {
@@ -30,22 +31,61 @@ app.get('/growit', function(req, res) {
     }
 });
 
-app.get('/functionaldunctional', function(req, res) {
+app.get('/functionalnative', function(req, res) {
+    var initial = { 'ATOM': 0, 'NTOZ': 0 };
     // do some sort of crazy functional thing here
-    var halves = R.groupBy(function(name) {
-        let ucname = name.toUpperCase();
-        return name[0] >= 'A' && name[0] <= 'M' ? 'ATOM' :
-               name[0] >= 'N' && name[0] <= 'Z' ? 'NTOZ' : 'UNKNOWN';
+    function reducer(memo, name) {
+      let ucname = name.toUpperCase();
+      if (ucname[0] >= 'A' && ucname[0] <= 'M') {
+        memo['ATOM'] = memo['ATOM'] + 1;
+      } else {
+        memo['NTOZ'] = memo['NTOZ'] + 1;
+      }
+      return memo;
+    }    
+    var result = names.reduce(reducer, initial, names);
+    res.send(200, result);
+  });
+
+
+app.get('/functionalramda', function(req, res) {
+    var initial = { 'ATOM': 0, 'NTOZ': 0 };
+    // do some sort of crazy functional thing here
+    function reducer(memo, name) {
+      let ucname = name.toUpperCase();
+      if (ucname[0] >= 'A' && ucname[0] <= 'M') {
+        memo['ATOM'] = memo['ATOM'] + 1;
+      } else {
+        memo['NTOZ'] = memo['NTOZ'] + 1;
+      }
+      return memo;
+    }    
+    var result = R.reducdocument.querySelector('selector');e(reducer, initial, names);
+    res.send(200, result);
+  });
+
+// TODO - overkill?
+app.get('/functionalasync', function(req, res) {
+  async.reduce(names, {'ATOM': 0, 'NTOZ': 0}, function(memo, name, callback) {
+   let ucname;
+   process.nextTick(function() {
+    ucname = name.toUpperCase();
+   });
+   process.nextTick(function() {
+    if (ucname[0] >= 'A' && ucname[0] <= 'M') {
+      memo['ATOM'] = memo['ATOM'] + 1;
+    } else if(ucname[0] >= 'N' && ucname[0] <= 'Z') {
+      memo['NTOZ'] = memo['NTOZ'] + 1;
+    }
+   });
+    process.nextTick(function() {
+      callback(null, memo);
     });
-    var remapped = R.map(result => { 
-        let keyCount = Object.keys(result).length;
-        return { key: keyCount};
-    });
-     setTimeout(() => { 
-        let groups = halves(names);
-        let results = remapped(groups);
-        res.send(200, results);
-     }, 100);
+  },
+  function(err, result) {
+    res.send(200, result);  
+  });
+  
 });
 
 app.listen(3000, function () {
