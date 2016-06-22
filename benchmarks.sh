d9e75c0486ebaf24e6f7b04e648d1e61116136c9
Ken Rimple <krimple@chariotsolutions.com>
2016-06-21 20:24:11 -0400

    moved logging to logs/ directory

--- benchmarks.sh
+++ benchmarks.sh
@@ -6,7 +6,7 @@ numparallel=6
 for i in "${benchmarks[@]}"
 do
     echo "starting benchmark $i..."
-    ab -n $numiters -c $numparallel -g graphdata/$i http://localhost:3000/$i > $i.out
+    ab -n $numiters -c $numparallel -g graphdata/$i http://localhost:3000/$i > logs/$i.out
     gnuplot settings/$i.gnuplot
     echo "finished benchmark $i"
 done
