0332dd87b57ac531f8f3926f95d11c40743ff12d
Ken Rimple <krimple@chariotsolutions.com>
2016-06-21 20:21:47 -0400

    first real move to benchmarking

---
+++ benchmarks.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+# declare an array called array and define 3 vales
+benchmarks=( functionalnative functionalramda functionalasync )
+numiters=180
+numparallel=6
+for i in "${benchmarks[@]}"
+do
+    echo "starting benchmark $i..."
+    ab -n $numiters -c $numparallel -g graphdata/$i http://localhost:3000/$i > $i.out
+    gnuplot settings/$i.gnuplot
+    echo "finished benchmark $i"
+done
+
